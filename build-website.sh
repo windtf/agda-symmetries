2c17232d88bf78c80e259d60d04ae6f427b7e05e
pufferffish <t.f.w.wong@vu.nl>
2025-10-28 16:36:55 +0100

    run create-everything first

--- build-website.sh
+++ build-website.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # This script is meant to be run by Cloudflare Pages to build the website
 
+chmod +x ./create-everything.sh
+./create-everything.sh
+
 wget https://github.com/agda/agda/releases/download/v2.7.0.1/Agda-v2.7.0.1-linux.tar.xz -O Agda.tar.xz
 tar -xf Agda.tar.xz
 export Agda_datadir=$(pwd)/Agda-v2.7.0.1/data
@@ -16,7 +19,4 @@ cd ~/.agda/cubical
 git checkout v0.6
 
 cd -
-chmod +x ./create-everything.sh
-./create-everything.sh
-
 ./agda --html --html-dir=public index.agda
