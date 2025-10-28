db7347a46e748df99f3065ae840984440c41b3ed
pufferffish <t.f.w.wong@vu.nl>
2025-10-28 15:51:44 +0100

    add website build script

---
+++ build-website.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This script is meant to be run by Cloudflare Pages to build the website
+
+pip install agda
+
+mkdir ~/.agda/
+echo $HOME/.agda/cubical/cubical.agda-lib > ~/.agda/libraries
+
+git clone https://github.com/agda/cubical ~/.agda/cubical
+cd ~/.agda/cubical
+git checkout v0.6
+
+cd -
+chmod +x ./create-everything.sh
+./create-everything.sh
+
+agda --html --html-dir=html index.agda
